63dc6c64bbae44d360eaa140ae889a8d15a046a0
Konstantin <konstantin.bay228@gmail.com>
2023-12-01 02:41:45 +0200

    update DigitalSignature v1

--- DigitalSignature/RSA/RSA.go
+++ DigitalSignature/RSA/RSA.go
@@ -2,31 +2,49 @@ package RSA
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
 	"math/big"
 )
 
-func GenKey() {
-	var n, g big.Int
+func GenKey(keyLength int) (sk1, pk1, p2 *big.Int) {
+	var p, q, n, d, e, fEuler *big.Int
+
 	var bigOne = big.NewInt(1)
-	//var bigNegOne = big.NewInt(-1)
-	var fEuler = big.NewInt(0)
-	//var g = big.NewInt(0)
 
-	p, _ := rand.Prime(rand.Reader, 512)
-	q, _ := rand.Prime(rand.Reader, 512)
+	for true {
+		p, _ = rand.Prime(rand.Reader, keyLength)
+		q, _ = rand.Prime(rand.Reader, keyLength)
 
-	n.Mul(p, q)
+		fEuler = new(big.Int).Mul(new(big.Int).Sub(p, bigOne), new(big.Int).Sub(q, bigOne))
+		e, _ = rand.Int(rand.Reader, fEuler)
 
-	fEuler = new(big.Int).Mul(new(big.Int).Sub(p, bigOne), new(big.Int).Sub(q, bigOne))
-	e, _ := rand.Int(rand.Reader, fEuler)
-	if e.Cmp(fEuler) == -1 && e.Cmp(1) == 1 && g.GCD(e, fEuler) == 1 {
-		E := e
+		if e.Cmp(fEuler) == -1 && e.Cmp(bigOne) == 1 && new(big.Int).GCD(nil, nil, e, fEuler).String() == "1" {
+			break
+		}
 	}
 
+	n = new(big.Int).Mul(p, q)
+	d = new(big.Int).ModInverse(e, fEuler)
+
+	return d, e, n
 }
 
-func Encrypt() {}
+func DigitSign(m string, e, n *big.Int) *big.Int {
+	hash := Hashing(m)
+	return new(big.Int).Exp(hash, e, n)
+}
 
-func Decrypt() {}
+func Verification(m string, c, d, n *big.Int) bool {
+	hashMess := new(big.Int).Exp(c, d, n)
+	hash := Hashing(m)
+	if hashMess.String() == hash.String() {
+		return true
+	} else {
+		return false
+	}
+}
 
-func ShareKey() {}
+func Hashing(m string) *big.Int {
+	hashMess := sha256.Sum256([]byte(m))
+	return new(big.Int).SetBytes(hashMess[:])
+}
--- DigitalSignature/main.go
+++ DigitalSignature/main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"DigitalSignature/RSA"
+	"bufio"
+	"fmt"
+	"math/big"
+	"os"
+)
+
+func TestDigSign() {
+	reader := bufio.NewReader(os.Stdin)
+	var bytes []byte
+	var mess1 string
+	fmt.Print("- Enter the message: ")
+	bytes, _, _ = reader.ReadLine()
+	mess1 = string(bytes)
+
+	var length int
+	fmt.Print("- Enter key length (256/512/1024) for GenKey: ")
+	fmt.Scanln(&length)
+
+	d, e, n := RSA.GenKey(length)
+	fmt.Println("- Private Key: "+d.Text(16)+" ; "+n.Text(16), "\n- Public Key: "+e.Text(16)+" ; "+n.Text(16)) // "\nn:", n.Text(16))
+
+	var sk1, pk1, p2 string
+	fmt.Print("- Enter 1 part of your Private Key(d): ")
+	fmt.Scanln(&sk1)
+
+	fmt.Print("- Enter 1 part of Public Key(e): ")
+	fmt.Scanln(&pk1)
+
+	fmt.Print("- Enter 2 part of keys(n): ")
+	fmt.Scanln(&p2)
+
+	e_pk1, _ := new(big.Int).SetString(pk1, 16)
+	n_p2, _ := new(big.Int).SetString(p2, 16)
+	d_sk1, _ := new(big.Int).SetString(sk1, 16)
+
+	c := RSA.DigitSign(mess1, e_pk1, n_p2)
+	fmt.Println("- Your Digital Signature:", c.Text(16))
+
+	var ds string
+	fmt.Print("- Enter your Digital Signature: ")
+	fmt.Scanln(&ds)
+
+	var mess2 string
+	fmt.Print("- Enter the message: ")
+	bytes, _, _ = reader.ReadLine()
+	mess2 = string(bytes)
+
+	cInt, _ := new(big.Int).SetString(ds, 16)
+
+	dec := RSA.Verification(mess2, cInt, d_sk1, n_p2)
+	fmt.Println("- Verification of DS is", dec, "!")
+}
+
 func main() {
-	//fmt.Printf("")
+	TestDigSign()
 }
